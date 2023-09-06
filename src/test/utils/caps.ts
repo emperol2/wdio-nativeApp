07aad6dff77523bca52c7c020bc2ff3634e618cb
Nuttapon Pichetpongsa <emperol2@gmail.com>
2023-09-06 22:54:22 +0700

    test

--- src/test/utils/caps.ts
+++ src/test/utils/caps.ts
@@ -1,21 +1,24 @@
+import { resolve } from "path";
+
 class Caps {
-
-    emulator =
-    [{
-      'appium:platformName': 'android',
-      'appium:deviceName': 'nexus',
-      'appium:app':'./src/test/api.apk',
-      'appium:platformVersion': '11',
-      'appium:uiautomator2ServerInstallTimeout': 150000,
-      'appium:appWaitForLaunch': true,
-      'appium:disableWindowAnimation': true,
-      'appium:autoGrantPermissions': true,
-      'appium:clearSystemFiles': true,
-      'appium:nativeWebScreenshot': true,
-      'appium:adbExecTimeout': 80000,
-      'appium:automationName': 'uiautomator2'
-    }]
-
+  emulator = [
+    {
+      "appium:platformName": "android",
+      // 'appium:deviceName': 'nexus',
+      "appium:deviceName": "Pixel 4 API 30",
+      "appium:app": resolve("./src/test/api.apk"),
+      "appium:platformVersion": "11",
+      // "appium:udid": "emulator-5556",
+      "appium:uiautomator2ServerInstallTimeout": 150000,
+      "appium:appWaitForLaunch": true,
+      "appium:disableWindowAnimation": true,
+      "appium:autoGrantPermissions": true,
+      "appium:clearSystemFiles": true,
+      "appium:nativeWebScreenshot": true,
+      "appium:adbExecTimeout": 80000,
+      "appium:automationName": "uiautomator2",
+    },
+  ];
 }
 
 export default new Caps();
